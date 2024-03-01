3147d2f4b2d0d9fa95803957b6a55ecbdd0f28e2
Piotr <pc58@interia.pl>
2024-03-01 12:48:34 +0100

    0.11 Testowanie

--- SalonSamochodowyApp/tests.py
+++ SalonSamochodowyApp/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+import unittest
 
-# Create your tests here.
+class ListCars(unittest.TestCase):
+    def setUp(self):
+        # Przygotowanie obiektu ListCars z odpowiednimi danymi dla testu
+        self.list_cars = ListCars()  # Tutaj utwórz obiekt ListCars zgodnie z implementacją aplikacji
+        self.list_cars.carImage = "path/to/image.jpg"  # Przypisz atrybut carImage, który chcesz przetestować
+
+    def test_inputExists(self):
+        # Sprawdź, czy atrybut carImage nie jest pusty
+        self.assertIsNotNone(self.list_cars.carImage)
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+# import unittest
+#
+# class ListCars(unittest.TestCase):
+#     def test_inputExists(self):
+#         self.assertIsNotNone(self.carImage)
+#
+# if __name__ == '__main__':
+#     unittest.main()
