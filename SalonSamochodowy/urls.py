c05c879da4c22ea8461047515393b647c87e25e4
Piotr <gdx156879@student.gdansk.merito.pl>
2024-03-13 16:35:23 +0100

    Sentry

--- SalonSamochodowy/urls.py
+++ SalonSamochodowy/urls.py
@@ -3,12 +3,22 @@ from django.urls import path
 from django.conf import settings
 from SalonSamochodowyApp.views import cars
 from django.conf.urls.static import static
+from django.urls import path
+
+def trigger_error(request):
+    division_by_zero = 1 / 0
+
+ name=input('Piotr')
+
+int('xyz')
+
+nt('xyz')
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', cars, name='cars'),
-]
-
+    # path('', cars, name='cars'),
+    path('sentry-debug/', trigger_error),
+              ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
