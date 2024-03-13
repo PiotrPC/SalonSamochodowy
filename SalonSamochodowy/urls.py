6453fb1df266fb975066ffdf2a90d6575376a903
Piotr <pc58@interia.pl>
2024-03-13 22:55:25 +0100

    Sentry poprawa

--- SalonSamochodowy/urls.py
+++ SalonSamochodowy/urls.py
@@ -3,12 +3,22 @@ from django.urls import path
 from django.conf import settings
 from SalonSamochodowyApp.views import cars
 from django.conf.urls.static import static
+from django.urls import path
+
+def trigger_error(request):
+    division_by_zero = 1 / 0
+
+# Piotr
+#
+# int('xyz')
+#
+# '2' + 2
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', cars, name='cars'),
-]
-
+    # path('', cars, name='cars'),
+    path('sentry-debug/', trigger_error),
+              ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
